3450cd5c64f56502a83b4a91462b5ed25672c1bf
sreeram051 <sreeram8122@gmail.com>
2024-10-01 00:15:01 +0530

    form completed

--- src/routes/sign-in/sign.in.component.jsx
+++ src/routes/sign-in/sign.in.component.jsx
@@ -1,14 +1,10 @@
 import LogInForm from "../../components/log-in-form/log-in.component";
 import SignUpForm from "../../components/sign-up-form/sign-up.component";
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
+import "./sign-in.styles.scss";
 
 const signIn = () => {
   return (
-    <div>
-      <h1>sign-in</h1>
+    <div className="sign-in">
       <LogInForm />
       <SignUpForm />
     </div>
