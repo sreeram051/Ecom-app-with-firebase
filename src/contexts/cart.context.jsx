d348b98cf923fb5f9fab7b719edf6e5d8052cd58
sreeram051 <sreeram8122@gmail.com>
2024-10-13 23:51:51 +0530

    adding context and adding carticons

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,21 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {};
+
 export const CartContext = createContext({
   isCartopen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  setCartItems: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
 
   const value = { isCartOpen, setIsCartOpen };
 
